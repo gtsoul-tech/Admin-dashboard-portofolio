fdea1533cf20fb926fed32d0b71bd8597e6fb5ac
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-05-25 14:57:25 +0300

    Added transparent gif

--- script.js
+++ script.js
@@ -10,9 +10,30 @@ squareLogo.addEventListener("click", ()=> {
         div.style.display= "none";
     }}
     );
-})
+});
 
+const themes = {
+    light: {
+        '--sidebar-bg-color': 'rgb(5, 120, 155)',
+        '--sidebar-color': 'white',
+        '--second-bg-color':'orange',
+        '--upper-color': 'black',
+        '--light-theme-shadow':'rgba(255, 255, 255, 0.4)',
+        '--main-bg-color':'lightgrey',
+        '--white-to-grey':'white'
+    },
+    dark: {
+        '--sidebar-bg-color': 'orange', /*teal*/
+        '--sidebar-color': '#121212', /*white*/
+        '--second-bg-color':'rgb(5, 120, 155)',/*orange*/
+        '--upper-color': '#121212', /*white*/
+        '--light-theme-shadow':'rgba(2, 2, 2, 0.4)',
+        '--white-to-grey':'#222222',
+        '--black-to-white':'white',
+        '--main-bg-color':'#222222'
+    },
+  };
 
-//let element = document.querySelector(':root');
-//element.style.setProperty("--sidebar-bg-color", '#014d4e');
-//element.style.setProperty("--sidebar-color", '#black');
+// let element = document.querySelector(':root');
+// console.log(element);
+// element.styleSheets[0].insertRule(':root {--sidebar-bg-color: orange;--sidebar-color: #121212;--second-bg-color:rgb(5, 120, 155); --upper-color: #121212;--light-theme-shadow:rgba(2, 2, 2, 0.4);--white-to-grey:#222222;--black-to-white:white;--main-bg-color:#222222;}');
