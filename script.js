bd512fe5968bd3fe92a133b7cbabd7c70c6f050a
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-05-25 16:04:50 +0300

    Themes and rotate

--- script.js
+++ script.js
@@ -11,29 +11,11 @@ squareLogo.addEventListener("click", ()=> {
     }}
     );
 });
-
-const themes = {
-    light: {
-        '--sidebar-bg-color': 'rgb(5, 120, 155)',
-        '--sidebar-color': 'white',
-        '--second-bg-color':'orange',
-        '--upper-color': 'black',
-        '--light-theme-shadow':'rgba(255, 255, 255, 0.4)',
-        '--main-bg-color':'lightgrey',
-        '--white-to-grey':'white'
-    },
-    dark: {
-        '--sidebar-bg-color': 'orange', /*teal*/
-        '--sidebar-color': '#121212', /*white*/
-        '--second-bg-color':'rgb(5, 120, 155)',/*orange*/
-        '--upper-color': '#121212', /*white*/
-        '--light-theme-shadow':'rgba(2, 2, 2, 0.4)',
-        '--white-to-grey':'#222222',
-        '--black-to-white':'white',
-        '--main-bg-color':'#222222'
-    },
-  };
-
-// let element = document.querySelector(':root');
-// console.log(element);
-// element.styleSheets[0].insertRule(':root {--sidebar-bg-color: orange;--sidebar-color: #121212;--second-bg-color:rgb(5, 120, 155); --upper-color: #121212;--light-theme-shadow:rgba(2, 2, 2, 0.4);--white-to-grey:#222222;--black-to-white:white;--main-bg-color:#222222;}');
+function setLightTheme() {
+    localStorage.setItem('theme', 'light_theme');
+    document.documentElement.className = 'light_theme';
+}
+function setDarkTheme() {
+    localStorage.setItem('theme', 'dark_theme');
+    document.documentElement.className = 'dark_theme';
+}
