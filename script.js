d2ceee9f20d914ac17f3e87344724ecd0ae05a80
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-05-25 16:39:09 +0300

    Mobile size

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const squareLogo = document.getElementById("square_logo");
 squareLogo.addEventListener("click", ()=> {
   let titles = document.querySelectorAll(".titles");
-  const body = document.querySelector("body");
   titles.forEach((div) => {
     if(div.style.display === "none"){
         div.style.display= "inline";
-
     }else{
+        console.log(div)
         div.style.display= "none";
     }}
     );
